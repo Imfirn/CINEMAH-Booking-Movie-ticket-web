f303e967d5f7b840e1c76438dcbc965f912784a3
KUNKANIT THONGKENG <92772306+Imfirn@users.noreply.github.com>
2021-12-10 00:24:16 +0700

    newww

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect, useRef } from "react";
-import * as FaIcons from "react-icons/fa";
-import { IconContext } from "react-icons";
-import './Search.css'
+// import React, { useState, useEffect, useRef } from "react";
+// import * as FaIcons from "react-icons/fa";
+// import { IconContext } from "react-icons";
+// // import './Search.css'
 
-const SearchBar = ({setSearchTerm}) => {
-    const [state, setState] = useState('');
-    const inital = useRef(true);
+// const SearchBar = ({setSearchTerm}) => {
+//     const [state, setState] = useState('');
+//     const inital = useRef(true);
 
-    useEffect(() => {
-        if (inital.current){
-            inital.current = false;
-            return;
-        }
+//     useEffect(() => {
+//         if (inital.current){
+//             inital.current = false;
+//             return;
+//         }
 
-        const timer = setTimeout(() => {
-            setSearchTerm(state);
-        },500)
+//         const timer = setTimeout(() => {
+//             setSearchTerm(state);
+//         },500)
 
-        return () => clearTimeout(timer)
-    },[setSearchTerm, state])
+//         return () => clearTimeout(timer)
+//     },[setSearchTerm, state])
 
-    return(
-        <div className='Wrapper'>
-            <div className='Content'>
-                <IconContext.Provider value={{ className: "search-icons" }}>
-                    <FaIcons.FaSearch className="icon" />
-                </IconContext.Provider>
-                <input 
-                    type='text' 
-                    className="input"
-                    placeholder='Serch Movie' 
-                    onChange={event => setState(event.currentTarget.value)}
-                    value={state}
-                />
-            </div>
-        </div>
-    );
-};
+//     return(
+//         <div className='Wrapper'>
+//             <div className='Content'>
+//                 <IconContext.Provider value={{ className: "search-icons" }}>
+//                     <FaIcons.FaSearch className="icon" />
+//                 </IconContext.Provider>
+//                 <input 
+//                     type='text' 
+//                     className="input"
+//                     placeholder='Serch Movie' 
+//                     onChange={event => setState(event.currentTarget.value)}
+//                     value={state}
+//                 />
+//             </div>
+//         </div>
+//     );
+// };
 
-export default SearchBar; 
+// export default SearchBar; 
--- src/components/pages/AllMovie/Movie.js
+++ src/components/pages/AllMovie/Movie.js
@@ -1,41 +1,75 @@
-import React,{useEffect,useState} from 'react';
-import Grid from '../../Grid/Grid'
-import "./Movie.css"
+import React, { useEffect, useState } from "react";
+import Grid from "../../Grid/Grid";
+import "./Movie.css";
 // import SearchBar from '../../Search/Search.js';
-import { useMovieFench } from '../../Hook/useMovieFench';
+// import { useMovieFench } from '../../Hook/useMovieFench';
 import axios from "axios";
 function Movie() {
-//   const { setSearchTerm } = useMovieFench(); 
+  //   const { searchTerm, setSearchTerm } = useMovieFench();
+  //   const [state, setState] = useState("");
   const [nameMovie, setNameMovie] = useState(null);
   const timeout = async (ms) => {
     return new Promise((res) => setTimeout(res, ms));
   };
+
+  const pushData = async (data) => {
+    let formField = new FormData();
+    formField.append("search", data);
+
+    await axios({
+      method: "post",
+      url: "http://127.0.0.1:8000/api/Search/",
+      data: formField,
+    }).then((response) => {});
+  };
+
   useEffect(() => {
     axios.get("http://127.0.0.1:8000/api/MovieLH/").then((res) => {
       setNameMovie(res.data);
     });
   }, []);
   if (!nameMovie) return "error";
-    return(
-        <div className={"home__hero-section darkBg"}>
-        <div className="Wrap-mov">
+  return (
+    <div className={"home__hero-section darkBg"}>
+      <div className="Wrap-mov">
+        <div className="Wrap-serach">
+          <input
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            onChange={async (e) => {
+              pushData(e.target.value);
+              await timeout(100);
+              if ((e.target.value == "")) {
+                axios
+                  .get("http://127.0.0.1:8000/api/Detail/")
+                  .then((res) => {
+                    setNameMovie(res.data);
+                  });
+              } else {
+                axios
+                  .get("http://127.0.0.1:8000/api/NameSearch/")
+                  .then((res) => {
+                    setNameMovie(res.data);
+                  });
+              }
+            }}
+          />
+        </div>
         <div className="allMovie">
-            {/* <SearchBar setSearchTerm={setSearchTerm} /> */}
-           
-            {nameMovie &&
-           nameMovie.map((item, index) => (
+          {nameMovie &&
+            nameMovie.map((item, index) => (
               <div key={index}>
-                 <Grid 
-                    name={item.Name}
-                    img={`/image/nowPic/Movie/${item.Name}/${item.Date}_1.jpg`}
-                 />               
-                
+                <Grid
+                  name={item.Name}
+                  img={`/image/nowPic/Movie/${item.Name}/${item.Date}_1.jpg`}
+                />
               </div>
             ))}
-            </div>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Movie
+export default Movie;
