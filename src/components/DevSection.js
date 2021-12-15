7feb42b4bf0fc29ea5df24fb5ef251711042b148
KUNKANIT THONGKENG <92772306+Imfirn@users.noreply.github.com>
2021-12-16 01:50:31 +0700

    newww

--- src/components/DevSection.js
+++ src/components/DevSection.js
@@ -7,49 +7,48 @@ function DevSection() {
   return (
     <div className={"home__hero-section darkBg"}>
       <div style={{ textAlign: "center" }}>
-          
         <div className="container">
-          <div className="pack">
-            <div className="txt">
-            Frontend Developer
-            </div>
           <div className="wrap-front">
+            <div className="dev-front">
+              <img className="logo-back" src="image/logo/react.png" alt="" />
+              <div className="txt">Frontend Developer</div>
+            </div>
             <div className="wrapper">
-            <NameCard
-              name={NameMember.front_1.name}
-              picMem={NameMember.front_1.pic}
-              id={NameMember.front_1.id}
-            />
-            <NameCard
-              name={NameMember.front_2.name}
-              picMem={NameMember.front_2.pic}
-              id={NameMember.front_2.id}
-            />
-            <NameCard
-              name={NameMember.front_3.name}
-              picMem={NameMember.front_3.pic}
-              id={NameMember.front_3.id}
-            />
+              <NameCard
+                name={NameMember.front_1.name}
+                picMem={NameMember.front_1.pic}
+                id={NameMember.front_1.id}
+              />
+              <NameCard
+                name={NameMember.front_2.name}
+                picMem={NameMember.front_2.pic}
+                id={NameMember.front_2.id}
+              />
+              <NameCard
+                name={NameMember.front_3.name}
+                picMem={NameMember.front_3.pic}
+                id={NameMember.front_3.id}
+              />
             </div>
           </div>
-          </div>
-          
+
           <div className="wrap-back">
-          <div className="txt2">
-            Frontend Developer
+            <div className="dev-back">
+              <img className="logo-back" src="image/logo/django.png" alt="" />
+              <div className="txt2">Backend Developer</div>
             </div>
-          <div className="wrapper">
-            <NameCard
-              name={NameMember.back_1.name}
-              picMem={NameMember.back_1.pic}
-              id={NameMember.back_1.id}
-            />
-            <NameCard
-              name={NameMember.back_2.name}
-              picMem={NameMember.back_2.pic}
-              id={NameMember.back_2.id}
-            />
-          </div>
+            <div className="wrapper">
+              <NameCard
+                name={NameMember.back_1.name}
+                picMem={NameMember.back_1.pic}
+                id={NameMember.back_1.id}
+              />
+              <NameCard
+                name={NameMember.back_2.name}
+                picMem={NameMember.back_2.pic}
+                id={NameMember.back_2.id}
+              />
+            </div>
           </div>
         </div>
       </div>
