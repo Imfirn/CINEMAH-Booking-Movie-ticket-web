0b9766eba166996a4faa9a2a01d4d3b8c848a657
KUNKANIT THONGKENG <92772306+Imfirn@users.noreply.github.com>
2021-12-20 23:20:40 +0700

    add select sort

--- src/components/pages/AllMovie/Movie.js
+++ src/components/pages/AllMovie/Movie.js
@@ -6,8 +6,8 @@ import "./Movie.css";
 import axios from "axios";
 function Movie() {
   //   const { searchTerm, setSearchTerm } = useMovieFench();
-  //   const [state, setState] = useState("");
-  const [nameMovie, setNameMovie] = useState(null);
+  const [sortState, setSortState] = useState("");
+  const [nameMovie, setNameMovie] = useState(null); 
   const timeout = async (ms) => {
     return new Promise((res) => setTimeout(res, ms));
   };
@@ -24,7 +24,7 @@ function Movie() {
   };
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/MovieLH/").then((res) => {
+    axios.get(`http://127.0.0.1:8000/api/Detail/`).then((res) => {
       setNameMovie(res.data);
     });
   }, []);
@@ -32,37 +32,63 @@ function Movie() {
   return (
     <div className={"home__hero-section darkBg"}>
       <div className="Wrap-mov">
-        <div className="Wrap-serach">
-          <input
-            className="ser"
-            id="outlined-basic"
-            label="Search"
-            placeholder="Enter Movie name"
-            variant="outlined"
-            onChange={async (e) => {
-              pushData(e.target.value);
-              await timeout(100);
-              if ((e.target.value == "")) {
-                axios              
-                  .get("http://127.0.0.1:8000/api/Detail/")
+        <div className="Wrap-type">
+          <div className="Wrap-serach">
+            <input
+              className="ser"
+              id="outlined-basic"
+              label="Search"
+              placeholder="Enter Movie name"
+              variant="outlined"
+              onChange={async (e) => {
+                pushData(e.target.value);
+                await timeout(100);
+                if (e.target.value == "") {
+                  axios.get("http://127.0.0.1:8000/api/Detail/").then((res) => {
+                    setNameMovie(res.data);
+                  });
+                } else {
+                  axios
+                    .get("http://127.0.0.1:8000/api/NameSearch/")
+                    .then((res) => {
+                      setNameMovie(res.data);
+                    });
+                }
+              }}
+            />
+          </div>
+          <div className="select-type">
+            <label htmlFor="movie-sort">Sorted By</label>
+            <select
+              onChange={(e) => {
+                const selectedSort = e.target.value;
+                setSortState(selectedSort);
+                if (selectedSort == "MovieLH") {
+                  axios
+                    .get("http://127.0.0.1:8000/api/MovieLH/")
+                    .then((res) => {
+                      setNameMovie(res.data);
+                    });
+                }else if(selectedSort == "MovieHL"){
+                  axios
+                  .get("http://127.0.0.1:8000/api/MovieHL/")
                   .then((res) => {
                     setNameMovie(res.data);
                   });
-              } else {
-                axios
-                  .get("http://127.0.0.1:8000/api/NameSearch/")
-                  .then((res) => {
-                    setNameMovie(res.data);
-                  });
-              }
-            }}
-          />
+
+                }
+              }}
+            >
+              <option value="MovieLH">Low to High </option>
+              <option value="MovieHL">High to Low </option>
+            </select>
+            {sortState}
+          </div>
         </div>
         <div className="allMovie">
           {nameMovie &&
             nameMovie.map((item, index) => (
               <div key={index}>
-                
                 <Grid
                   name={item.Name}
                   img={`/image/nowPic/Movie/${item.Name}/${item.Date}_1.jpg`}
